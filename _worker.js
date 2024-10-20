840337ca60f6b3d2b564dc4fdbe4bab9fe0ed3f8
hubin <8538232+wuliangshangren@users.noreply.github.com>
2024-10-21 00:57:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'ccf82ca9-3e8e-48c0-af3c-12cb54440d80';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
